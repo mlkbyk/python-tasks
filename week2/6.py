1af8a99df61fa9614df3691c8514aedc5636a4a3
mlkbyk <166039308+mlkbyk@users.noreply.github.com>
2025-12-02 12:00:23 +0300

    Add script to sum even numbers and count down

---
+++ week2/6.py
@@ -0,0 +1,11 @@
+
+
+# a) 1-100 arası çift sayıların toplamı
+toplam = sum(range(2, 101, 2))
+print(f"Çift sayıların toplamı: {toplam}")
+
+print("-" * 10)
+
+# b) 50'den 40'a geriye yazdırma
+for i in range(50, 39, -1):
+    print(i)
